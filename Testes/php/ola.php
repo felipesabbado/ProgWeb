06d8f0c65a66a37e1aa0615c33da496ec62b2620
Felipe Campelo Sabbado <fsabbado@gmail.com>
2022-04-09 17:11:19 +0100

    teste02

--- Testes/php/ola.php
+++ Testes/php/ola.php
@@ -3,11 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <title>PHP</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
+
 <body>
-    <h1>Testando PHP</h1>
+<header>
+    <h1>Testando PHP - Header</h1>
+</header>
+
+<aside>
+    <h1>Aside</h1>
+</aside>
+
+<main>
+    <h1>Body</h1>
     <?php
-        echo "Olá, Mundo!";
+    echo "Olá, <strong>Betão</strong>!<br>The current date is ";
+    echo date("l d F, Y");
     ?>
+</main>
+
+<footer>
+    <h1>Footer</h1>
+</footer>
 </body>
 </html>
