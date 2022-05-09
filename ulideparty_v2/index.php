19b10e814badd12d75d95bc4c3823d6ae1f00915
Felipe <fsabbado@gmail.com>
2022-05-09 21:43:30 +0100

    header

---
+++ ulideparty_v2/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title Page-->
+    <title>Ulide Party</title>
+</head>
+<header>
+    <?php include 'menu.php' ?>
+</header>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="card" id="bares">
+                <img src="img/home_bares.jpg" alt="Bares">
+                <div class="card-img-overlay cardtype">
+                    <h1>Bares</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" id="restaurantes">
+                <img src="img/home_restaurantes.jpg" alt="Restaurantes">
+                <div class="card-img-overlay cardtype">
+                    <h1>Restaurantes</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" id="discotecas">
+                <img src="img/home_discotecas.jpg" alt="Discotecas">
+                <div class="card-img-overlay cardtype">
+                    <h1>Discotecas</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<footer>
+<h1>Footer</h1>
+</footer>
+</html>
